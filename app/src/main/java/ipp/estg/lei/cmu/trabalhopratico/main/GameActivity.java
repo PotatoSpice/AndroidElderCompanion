5bb43fae527138a8cfe79c70dcd69018ddf0a8cd
SterbenCyrodiil <rodrigo.coelho1999@gmail.com>
2020-01-27 18:21:30 +0000

    [Branch Update] Notificações Diárias adicionadas. TODO: Melhorias no jogo

--- app/src/main/java/ipp/estg/lei/cmu/trabalhopratico/main/GameActivity.java
+++ app/src/main/java/ipp/estg/lei/cmu/trabalhopratico/main/GameActivity.java
@@ -3,7 +3,13 @@ package ipp.estg.lei.cmu.trabalhopratico.main;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Calendar;
+import java.util.Date;
 
 import ipp.estg.lei.cmu.trabalhopratico.R;
 
@@ -32,4 +38,23 @@ public class GameActivity extends AppCompatActivity implements GameEntranceFragm
         transaction.commit();
     }
 
+
+    public void notificationAlarm() {
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 11);
+
+        if (calendar.getTime().compareTo(new Date()) < 0) {
+            calendar.add(Calendar.DAY_OF_MONTH, 1);
+        }
+        Intent intent = new Intent(getApplicationContext(), NotifcationBroadcastReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        if (alarmManager != null) {
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
+
+        }
+
+    }
 }
---
+++ app/src/main/java/ipp/estg/lei/cmu/trabalhopratico/main/NotifcationBroadcastReceiver.java
@@ -0,0 +1,13 @@
+package ipp.estg.lei.cmu.trabalhopratico.main;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class NotifcationBroadcastReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            NotificationClass notificationHelper = new NotificationClass(context);
+            notificationHelper.createNotification();
+        }
+}
